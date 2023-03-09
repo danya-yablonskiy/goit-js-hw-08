dfef4cda0084407854caf93e9586c77aba3a72bd
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-03-09 23:55:30 +0200

    02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,7 +10,14 @@ const time = player.on('timeupdate', throttle(function(data) {
     localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
 }, 1000));
 
-const array = JSON.parse(localStorage.getItem('videoplayer-current-time'));
-const currentTime = array['seconds'];
 
-player.setCurrentTime(currentTime);
+
+currentTimeFn();
+
+function currentTimeFn(){
+    const array = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+    if(array){
+        const currentTime = array['seconds'];
+        player.setCurrentTime(currentTime);
+    }
+}
